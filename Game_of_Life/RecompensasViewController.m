16b98f100929efb7fb3b38422ebf7ae67f42d47d
levimoraes <Levi.Moraes>
2014-06-02 14:42:17 -0300

    Criacao das classes Perfil,Atividade e Recompensas

---
+++ Game_of_Life/RecompensasViewController.m
@@ -0,0 +1,38 @@
+//
+//  RecompensasViewController.m
+//  Game_of_Life
+//
+//  Created by Levi Santos on 02/06/14.
+//  Copyright (c) 2014 Levi Santos. All rights reserved.
+//
+
+#import "RecompensasViewController.h"
+
+@interface RecompensasViewController ()
+
+@end
+
+@implementation RecompensasViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
